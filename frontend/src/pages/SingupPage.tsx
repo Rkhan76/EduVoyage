06b88f46818a6f5b51070c3de51e0430bb4d66a1
Mohd Rakhshan khan <rakshaankhan1@gmail.com>
2024-09-29 02:45:21 +0530

    change the signupPage image according to route

--- frontend/src/pages/SingupPage.tsx
+++ frontend/src/pages/SingupPage.tsx
@@ -1,25 +1,29 @@
-import React from "react"
+import React from 'react'
 import MindBulbImg from '../assets/signin-side-image.png'
-import SingupContainer from "../container/SignupContainer"
+import TeacherImage from '../assets/teacherImage.png'
+import SignupContainer from '../container/SignupContainer'
+import { useLocation } from 'react-router-dom'
 
+const SignupPage: React.FC = () => {
+  const location = useLocation()
+  console.log(location.pathname) 
 
-const SignupPage: React.FC = ()=>{
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="flex w-full max-w-6xl">
-          <div className="w-1/2 p-6">
-            <img
-              src={MindBulbImg}
-              alt="bulb image showing brain"
-              className="w-full h-auto rounded-lg"
-            />
-          </div>
-          <div className="w-1/2 p-6 flex items-center">
-           <SingupContainer/>
-          </div>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex w-full max-w-6xl">
+        <div className="w-1/2 p-6">
+          <img
+            src={location.pathname === '/signup' ? MindBulbImg : TeacherImage}
+            alt="bulb image showing brain"
+            className="w-full h-auto rounded-lg"
+          />
+        </div>
+        <div className="w-1/2 p-6 flex items-center">
+          <SignupContainer />
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default SignupPage
